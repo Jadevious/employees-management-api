ec836aa338a874bbcf974dc1a23fe5b80796fd71
Jade Naughton <jade.naughton@kainos.com>
2022-09-28 15:35:02 +0100

    Added Junit and mockit for unit testing

---
+++ src/test/com/kainos/ea/RecruitmentRequestServiceTest.java
@@ -0,0 +1,13 @@
+package com.kainos.ea;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecruitmentRequestServiceTest {
+
+    @Test
+    void getMsg() {
+
+    }
+}
---
+++ src/test/com/kainos/ea/db/JobsDbTest.java
@@ -0,0 +1,12 @@
+package com.kainos.ea.db;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JobsDbTest {
+
+    @Test
+    void getConnection() {
+    }
+}
