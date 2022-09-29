6a8d3c6ff86e884cad4b21842e04bcf0dc78ee27
Jade Naughton <jade.naughton@kainos.com>
2022-09-29 10:06:22 +0100

    Reverted to older version of mockito

--- src/test/com/kainos/ea/service/JobsRequestServiceTest.java
+++ src/test/com/kainos/ea/service/JobsRequestServiceTest.java
@@ -1,11 +1,18 @@
 package com.kainos.ea.service;
 
 import com.kainos.ea.dao.JobsDao;
+import com.kainos.ea.exception.DatabaseConnectionException;
+import com.kainos.ea.models.Job;
 import com.kainos.ea.util.DatabaseConnector;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -17,8 +24,21 @@ class JobsRequestServiceTest {
 
     JobsRequestService jobsRequestService = new JobsRequestService(jobsDao, databaseConnector);
 
-    @Test
-    void getJobs_shouldReturnListOfJobs_whenDaoReturnsListOfJobs() {
+    Connection conn;
 
+    @Test
+    void getJobs_shouldReturnListOfJobs_whenDaoReturnsListOfJobs() throws DatabaseConnectionException, SQLException {
+        List<Job> before = new ArrayList<Job>();
+        before.add(new Job(
+                1,
+                "Software Engineer",
+                "Develops Software for Kainos"
+        ));
+        Mockito.when(databaseConnector.getConnection()).thenReturn(conn);
+        Mockito.when(jobsDao.getJobs(conn)).thenReturn(before);
+
+        List<Job> after = jobsRequestService.getJobs();
+
+        assertEquals(after, before);
     }
 }
