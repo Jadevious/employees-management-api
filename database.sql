a23d8ac5338481f3da63aa28a3fadddf2f3d1e3c
Conor McGee <conor.mcgee@kainos.com>
2022-10-05 11:53:55 +0100

    changing job prefix on description

--- database.sql
+++ database.sql
@@ -4,6 +4,6 @@ USE groupc_kainos_recruitment_JenniferC;
 CREATE TABLE IF NOT EXISTS job_roles (
 id smallint PRIMARY KEY AUTO_INCREMENT,
 name varchar(50) NOT NULL,
-job_description varchar(500) NOT NULL,
+description varchar(500) NOT NULL,
 capability varchar(50)
 );
